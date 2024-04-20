9b9605f81eb3d751de7b161a9e6fbe38238ff837
Dunna, Kranthi Kumar <kranthiwork08@gmail.com>
2024-04-20 03:30:22 -0400

    Azure

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const { add } = require("./arithmetica");
+app.use(cors());
 
 app.use(cors());
 
+if (!process.env.PORT) {
+  throw new Error(
+    "Please specify the sport number for the HTTP server with the environment variable PORT."
+  );
+}
 
-app.get('/add/:n/:m', (req, res) => {
-    const n = Number(req.params.n);
-    const m = Number(req.params.m);
-    const sum = n + m;
-    console.log(`Adding ${n} and ${m} to get ${sum}`);
+const port = process.env.PORT;
+app.get("/", (req, res) => {
+  res.send("Arithmetic service – last updated 3/5/2022");
+});
 
-    if (!isNaN(n) && !isNaN(m)) {
-        res.json(sum);
-    } else {
-        res.status(400).send('Invalid input: Please ensure both parameters are numbers.');
-    }
+app.get("/add/:n/:m", (req, res) => {
+  const n = Number(req.params.n);
+  const m = Number(req.params.m);
+  const sum = n + m;
+  console.log(`Adding ${n} and ${m} to get ${sum}`);
+
+  if (!isNaN(n) && !isNaN(m)) {
+    res.json(sum);
+  } else {
+    res
+      .status(400)
+      .send("Invalid input: Please ensure both parameters are numbers.");
+  }
 });
 
 app.listen(port, () => {
-    console.log("Server is running at http://localhost:" + port + "/add/");
+  console.log("Server is running at http://localhost:" + port + "/add/");
 });
